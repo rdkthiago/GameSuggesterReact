f33a868e2b62deb3ffa47794ab70e900c2c7f649
rdkthiago <rdkthiago19@gmail.com>
2026-01-24 16:55:11 -0300

    feat: setup basic API integration to fetch and display game name

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
+import axios from 'axios';
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [game, setGame] = useState(null);
+
+  async function handleGetSuggestion() {
+    const response = await axios.get("http://localhost:8080/api/games/suggestion");
+    console.log(response.data);
+    setGame(response.data[0]);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Game Suggester</h1>
+      <button onClick={handleGetSuggestion}>Obter Sugestão</button>
+      {game && <div><h2>{game.name}</h2></div>}
     </>
   )
 }
